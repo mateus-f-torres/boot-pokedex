52a67c229ceae2b1d43175041f319477374c342d
mateus-f-torres <mateus-f-torres@pm.me>
2024-09-20 16:16:15 -0300

    feat: brute force implementation of 'explore' command

--- internal/commands/command_explore.go
+++ internal/commands/command_explore.go
@@ -1,10 +1,71 @@
 package commands
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+
+	httpclient "github.com/mateus-f-torres/boot_pokedex/internal/http-client"
+)
+
+const LOCATION_URL string = "https://pokeapi.co/api/v2/location"
+
+type LocationDetails struct {
+	// ID    int    `json:"id"`
+	// Name  string `json:"name"`
+	Areas []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"areas"`
+}
+
+type AreaDetails struct {
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+			// URL  string `json:"url"`
+		} `json:"pokemon"`
+	} `json:"pokemon_encounters"`
+}
 
 func commandExplore(c *config, args []string) error {
 	if len(args) > 0 {
-		fmt.Printf("exploring: %v\n", args[0])
+		fmt.Printf("exploring %v...\n", args[0])
+	}
+
+	res, err := httpclient.Get(LOCATION_URL + "/" + args[0])
+	if err != nil {
+		return err
+	}
+
+	var locDetails LocationDetails
+	err = json.Unmarshal(res, &locDetails)
+	if err != nil {
+		return err
+	}
+
+	pokemons := map[string]bool{}
+	for _, item := range locDetails.Areas {
+		var areaDetails AreaDetails
+		rawArea, err := httpclient.Get(item.URL)
+		if err != nil {
+			return nil
+		}
+
+		err = json.Unmarshal(rawArea, &areaDetails)
+		if err != nil {
+			return nil
+		}
+
+		for _, encounter := range areaDetails.PokemonEncounters {
+			name := encounter.Pokemon.Name
+			if _, ok := pokemons[name]; !ok {
+				pokemons[name] = true
+			}
+		}
+	}
+
+	for k := range pokemons {
+		fmt.Printf(" - %v\n", k)
 	}
 
 	return nil
