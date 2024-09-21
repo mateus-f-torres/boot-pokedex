c20baa633b9f696d8026286cf70f32129c03d2f0
mateus-f-torres <mateus-f-torres@pm.me>
2024-09-21 10:53:43 -0300

    feat: improve help message with padding and better order

--- internal/commands/command_help.go
+++ internal/commands/command_help.go
@@ -2,6 +2,8 @@ package commands
 
 import (
 	"fmt"
+	"slices"
+	"strings"
 
 	"github.com/mateus-f-torres/boot_pokedex/internal/pokeapi"
 )
@@ -10,8 +12,26 @@ func commandHelp(api *pokeapi.PokeAPI, args []string) error {
 	fmt.Print("Welcome to the Pokedex!\nUsage:\n\n")
 	cmds := GetCommands()
 
-	for _, c := range cmds {
-		fmt.Printf("%v: %v\n", c.name, c.description)
+	pad := 0
+	slc := []Command{}
+	for _, v := range cmds {
+		if len(v.name) > pad {
+			pad = len(v.name)
+		}
+
+		slc = append(slc, v)
+	}
+
+	slices.SortFunc(slc, func(a, b Command) int {
+		return a.order - b.order
+	})
+
+	for _, v := range slc {
+		name := v.name
+		if len(name) < pad {
+			name = name + strings.Repeat(" ", pad-len(name))
+		}
+		fmt.Printf("  %v   %v\n", name, v.description)
 	}
 
 	return nil
--- internal/commands/get_commands.go
+++ internal/commands/get_commands.go
@@ -3,6 +3,7 @@ package commands
 import "github.com/mateus-f-torres/boot_pokedex/internal/pokeapi"
 
 type Command struct {
+	order       int
 	name        string
 	description string
 	Callback    func(api *pokeapi.PokeAPI, args []string) error
@@ -11,28 +12,39 @@ type Command struct {
 func GetCommands() map[string]Command {
 	return map[string]Command{
 		"map": {
+			order:       0,
 			name:        "map",
-			description: "Print the names of the next 20 locations in the Pokemon world",
+			description: "print the names of the next 20 locations in the Pokemon world",
 			Callback:    commandMap,
 		},
 		"mapb": {
+			order:       1,
 			name:        "mapb",
-			description: "Print the names of the previous 20 locations in the Pokemon world",
+			description: "print the names of the previous 20 locations in the Pokemon world",
 			Callback:    commandMapB,
 		},
 		"explore": {
-			name:        "explore",
-			description: "Print the names of all Pokémon in a given area",
+			order:       2,
+			name:        "explore <location>",
+			description: "explore a location and print list of available Pokémon",
 			Callback:    commandExplore,
 		},
+		"catch": {
+			order:       3,
+			name:        "catch <pokemon>",
+			description: "try to catch a given Pokémon",
+			Callback:    commandCatch,
+		},
 		"help": {
+			order:       4,
 			name:        "help",
-			description: "Displays a help message",
+			description: "displays a help message",
 			Callback:    commandHelp,
 		},
 		"exit": {
+			order:       5,
 			name:        "exit",
-			description: "Exit the Pokedex",
+			description: "exit the Pokedex",
 			Callback:    commandExit,
 		},
 	}
